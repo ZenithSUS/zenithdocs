073a7bcea01218eae0a403bb4f627e0c3eebdcf8
ZenithSUS <jeranmerino147@gmail.com>
2026-02-17 21:18:09 +0800

    remove: error checking if not defined.

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -1,19 +1,13 @@
 const isDev = process.env.NODE_ENV === "development";
 
 const config = {
-  API_URL: isDev
-    ? process.env.NEXT_PUBLIC_API_URL_DEV
-    : process.env.NEXT_PUBLIC_API_URL,
+  api: {
+    baseUrl: isDev
+      ? process.env.NEXT_PUBLIC_API_URL_DEV
+      : process.env.NEXT_PUBLIC_API_URL || "",
+    key: process.env.NEXT_PUBLIC_API_KEY || "",
+  },
 };
-
-const requiredEnvVars = ["NEXT_PUBLIC_API_URL", "NEXT_PUBLIC_API_URL_DEV"];
-
-// Check if the required environment variables are set
-requiredEnvVars.forEach((envVar) => {
-  if (!process.env[envVar]) {
-    throw new Error(`Missing environment variable: ${envVar}`);
-  }
-});
 
 export type Config = typeof config;
 
