a7addd2f18d8d83e48e0b7def1b22f7854693186
ZenithSUS <jeranmerino147@gmail.com>
2026-02-17 21:22:19 +0800

    create: tanstack query client configuration.

---
+++ client/src/lib/tanstack.ts
@@ -0,0 +1,19 @@
+import { QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 24 * 60 * 60 * 1000, // 24 hours
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+export default queryClient;
