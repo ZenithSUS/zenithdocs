b8e8394fcdd10c8a12371612314e4115b57e2db8
ZenithSUS <jeranmerino147@gmail.com>
2026-02-17 21:19:12 +0800

    add: x-api-key for security enhancement.

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 import config from "@/config/env";
 
 const api = axios.create({
-  baseURL: config.API_URL,
+  baseURL: config.api.baseUrl,
   headers: {
     "Content-Type": "application/json",
+    "x-api-key": config.api.key,
   },
 });
 
