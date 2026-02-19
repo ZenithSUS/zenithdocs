0a914dd08388707b8635fd2a855116cc1de4ded9
ZenithSUS <jeranmerino147@gmail.com>
2026-02-19 09:53:12 +0800

    refactor: remove unused user authentication and creation services, improve error handling in user retrieval

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,84 +1,27 @@
-import config from "../config/env.js";
 import { IUser } from "../models/User.js";
 import {
-  createUser,
   deleteUser,
   getAllUsers,
   getUserByEmail,
   getUserById,
   updateUser,
 } from "../repositories/user.repository.js";
-import { comparePassword, hashPassword } from "../utils/bcrypt-password.js";
-import jwt from "jsonwebtoken";
-
-/**
- * Login user
- * @param email - User email
- * @param password - User password
- * @returns User if found, null otherwise
- */
-export const loginService = async (email: string, password: string) => {
-  const user = await getUserByEmail(email);
-
-  if (!user) throw new Error("Invalid credentials");
-
-  const isMatch = await comparePassword(password, user.password!);
-
-  if (!isMatch) throw new Error("Invalid credentials");
-
-  // Generate access token and refresh token
-  const accessToken = jwt.sign(
-    { sub: user._id, role: user.role },
-    config.jwt.accessSecret,
-    { expiresIn: "1h" },
-  );
-
-  const refreshToken = jwt.sign(
-    { userId: user._id },
-    config.jwt.refreshSecret,
-    {
-      expiresIn: "7d",
-    },
-  );
-
-  // Update user refresh token in DB
-  user.refreshToken = refreshToken;
-  await user.save();
-
-  return {
-    user: {
-      id: user._id,
-      email: user.email,
-      role: user.role,
-    },
-    accessToken,
-    refreshToken,
-  };
-};
-
-/**
- * Create a new user
- * @param data - User data to create
- * @returns Created user
- */
-export const createUserService = async (data: Partial<IUser>) => {
-  // Encrypt password if provided
-  if (data.password) {
-    data.password = await hashPassword(data.password);
-  }
-
-  const user = await createUser(data);
-  return user;
-};
+import AppError from "../utils/app-error.js";
 
 /**
  * Get user by ID
  * @param id - User ID
  * @returns User if found, null otherwise
+ * @throws {Error} error if not full
  *
  */
 export const getUserByIdService = async (id: string) => {
+  if (!id) throw new AppError("User ID is required", 400);
+
   const user = await getUserById(id);
+
+  if (!user) throw new AppError("User not found", 404);
+
   return user;
 };
 
@@ -104,7 +47,7 @@ export const getUserByEmailService = async (email: string) => {
 /**
  * Update user by ID
  * @param id - User ID
- * @param data - Data to update
+ * @param {Partial<IUser>} data - Data to update
  * @returns Updated user if found, null otherwise
  *
  */
