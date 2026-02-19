4f666ac662773498d1687b85bcbd1022cf3f1862
ZenithSUS <jeranmerino147@gmail.com>
2026-02-19 23:51:59 +0800

    feat: update protect middleware to check user existence and handle async operations

--- server/src/middlewares/protect.middleware.ts
+++ server/src/middlewares/protect.middleware.ts
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 import config from "../config/env.js";
 import AppError from "../utils/app-error.js";
 import { JwtPayload } from "../types/express.js";
+import { getUserById } from "../repositories/user.repository.js";
 
-const protect = (req: Request, res: Response, next: NextFunction) => {
+const protect = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -16,10 +17,23 @@ const protect = (req: Request, res: Response, next: NextFunction) => {
   try {
     const decoded = jwt.verify(token, config.jwt.accessSecret) as JwtPayload;
 
+    // Check if user exists
+    const user = await getUserById(decoded.sub);
+    if (!user) throw new AppError("Unauthorized access", 401);
+
     req.user = decoded;
+
     next();
   } catch (error) {
-    return next(new AppError("Invalid or expired token", 403));
+    if (error instanceof jwt.JsonWebTokenError) {
+      return next(new AppError("Invalid token", 401));
+    }
+
+    if (error instanceof jwt.TokenExpiredError) {
+      return next(new AppError("Token expired", 401));
+    }
+
+    return next(error);
   }
 };
 
