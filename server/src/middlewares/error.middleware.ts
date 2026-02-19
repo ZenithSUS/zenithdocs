289fd19a294c760066147bfcfd54cde7c7c1602d
ZenithSUS <jeranmerino147@gmail.com>
2026-02-19 09:51:24 +0800

    refactor: enhance error handling middleware by removing unused code and improving type definitions

--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -1,26 +1,37 @@
 import { Request, Response, NextFunction } from "express";
-import colors from "../utils/log-colors.js";
+import AppError from "../utils/app-error.js";
 
-interface AppError extends Error {
-  statusCode?: number;
-}
-
+/**
+ * Error handling middleware
+ * @param {Error | AppError} err - Error object
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {NextFunction} next - Express next function
+ * @returns {void}
+ * @description
+ * If the error is an instance of AppError, it will
+ * return a JSON response with the error message and
+ * status code. If the error is not an instance of
+ * AppError, it will return a JSON response with a
+ * generic "Internal Server Error" message and a status
+ * code of 500.
+ */
 const errorHandler = (
-  err: AppError,
+  err: Error | AppError,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
 
-  console.log("=".repeat(50));
-  console.log(`${colors.red}Error: ${colors.reset}${err.message}`);
-  console.log("=".repeat(50));
-
-  res.status(statusCode).json({
+  res.status(500).json({
     success: false,
-    message: err.message || "Internal Server Error",
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    message: "Internal Server Error",
   });
 };
 
