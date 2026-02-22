6364e47c47c13f527fe4385634bee87cc74805d2
ZenithSUS <jeranmerino147@gmail.com>
2026-02-23 00:02:44 +0800

    rename: getUsageByUserAndMonth to fetchUsageByUserAndMonth

--- client/src/features/usage/useUsage.ts
+++ client/src/features/usage/useUsage.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import usageKeys from "./usage.key";
-import { getUsageByUserAndMonth } from "./usage.api";
+import { fetchUsageByUserAndMonth } from "./usage.api";
 
 const useUsage = (userId: string, month: string) => {
   const usageByUserAndMonth = useQuery({
     queryKey: usageKeys.byUserAndMonth(userId, month),
-    queryFn: () => getUsageByUserAndMonth(userId, month),
+    queryFn: () => fetchUsageByUserAndMonth(userId, month),
     enabled: !!userId && !!month,
   });
 
