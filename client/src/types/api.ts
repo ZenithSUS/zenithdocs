6a82ba473a22991015fffa57cb8b1e1201285de7
ZenithSUS <jeranmerino147@gmail.com>
2026-02-22 12:01:12 +0800

    feat: create expected types in the response in the server.

--- client/src/types/api.ts
+++ client/src/types/api.ts
@@ -14,8 +14,31 @@ export type AxiosError = {
   };
 };
 
+export type Pagination = {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+};
+
 export type ResponseWithUser = {
   success: boolean;
   message: string;
   data: User;
+};
+
+export type ResponseWithData<T> = {
+  success: boolean;
+  message: string;
+  data: T;
+};
+
+export type ResponseWithPagedData<T, K extends string> = {
+  success: boolean;
+  message: string;
+  data: {
+    [P in K]: T[];
+  } & {
+    pagination: Pagination;
+  };
 };
