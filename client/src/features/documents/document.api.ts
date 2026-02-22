e048e271b371e2475c5843d4567aa177ddb44444
ZenithSUS <jeranmerino147@gmail.com>
2026-02-22 23:46:43 +0800

    feat: rename data prop to res and add api crud methods.

--- client/src/features/documents/document.api.ts
+++ client/src/features/documents/document.api.ts
@@ -2,12 +2,17 @@ import api from "@/lib/axios";
 import { ResponseWithData, ResponseWithPagedData } from "@/types/api";
 import Doc from "@/types/doc";
 
-export const getDocumentByUserPaginated = async (
+export const createDocument = async (data: Partial<Doc>) => {
+  const { data: res } = await api.post("/api/documents", data);
+  return res;
+};
+
+export const fetchDocumentByUserPaginated = async (
   userId: string,
   page: number,
   limit: number,
 ) => {
-  const { data } = await api.get<ResponseWithPagedData<Doc, "documents">>(
+  const { data: res } = await api.get<ResponseWithPagedData<Doc, "documents">>(
     `/api/documents/user/${userId}`,
     {
       params: {
@@ -17,10 +22,27 @@ export const getDocumentByUserPaginated = async (
     },
   );
 
-  return data.data;
+  return res.data;
 };
 
-export const getDocumentById = async (id: string) => {
-  const { data } = await api.get<ResponseWithData<Doc>>(`/api/documents/${id}`);
-  return data.data;
+export const fetchDocumentById = async (id: string) => {
+  const { data: res } = await api.get<ResponseWithData<Doc>>(
+    `/api/documents/${id}`,
+  );
+  return res.data;
+};
+
+export const updateDocumentById = async (id: string, data: Partial<Doc>) => {
+  const { data: res } = await api.put<ResponseWithData<Doc>>(
+    `/api/documents/${id}`,
+    data,
+  );
+  return res.data;
+};
+
+export const deleteDocumentById = async (id: string) => {
+  const { data: res } = await api.delete<ResponseWithData<Doc>>(
+    `/api/documents/${id}`,
+  );
+  return res.data;
 };
