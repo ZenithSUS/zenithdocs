6944add5ddcb647d9ec9734092197018ccf8fa09
ZenithSUS <jeranmerino147@gmail.com>
2026-02-25 00:25:35 +0800

    modfiy: expected type value in document.

--- client/src/types/summary.ts
+++ client/src/types/summary.ts
@@ -2,7 +2,14 @@ export type SummaryType = "short" | "bullet" | "detailed" | "executive";
 
 export interface Summary {
   _id: string;
-  document: string;
+  document:
+    | string
+    | {
+        _id: string;
+        title: string;
+        fileType: string;
+        fileSize: number;
+      };
   type: SummaryType;
   content: string;
   tokensUsed: number;
