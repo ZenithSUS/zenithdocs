ede45f58a7e9ee8cd0172dc943972957fe115911
ZenithSUS <jeranmerino147@gmail.com>
2026-02-25 00:36:05 +0800

    feat: create new folder modal with hook handling the data.

---
+++ client/src/components/dashboard/modals/NewFolderModal.tsx
@@ -0,0 +1,117 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import useFolder from "@/features/folder/useFolder";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback, useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+interface NewFolderModalProps {
+  userId: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const folderSchema = z.object({
+  name: z.string().min(1, "Folder name is required."),
+});
+
+type FolderForm = z.infer<typeof folderSchema>;
+
+const NewFolderModal = ({ userId, open, setOpen }: NewFolderModalProps) => {
+  const { createFolderMutation } = useFolder();
+  const {
+    mutateAsync: createFolder,
+    isPending,
+    isError,
+    error,
+  } = createFolderMutation(userId);
+
+  const { register, handleSubmit, formState, reset } = useForm<FolderForm>({
+    resolver: zodResolver(folderSchema),
+  });
+
+  const createNewFolder = useCallback(async (data: FolderForm) => {
+    try {
+      await createFolder({
+        name: data.name,
+        user: userId,
+      });
+
+      reset();
+      toast.success("Folder created successfully!");
+      setOpen(false);
+    } catch (error) {
+      console.log("Error creating folder:", error);
+    }
+  }, []);
+
+  const errorMessage = useMemo(
+    () =>
+      error?.response.data.message || "Something went wrong. Please try again.",
+    [],
+  );
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent className="bg-background">
+        <DialogHeader>
+          <DialogTitle>Create Folder</DialogTitle>
+          <DialogDescription>
+            Enter the name of the new folder
+          </DialogDescription>
+        </DialogHeader>
+        <form
+          onSubmit={handleSubmit(createNewFolder)}
+          className="flex flex-col gap-2"
+        >
+          <Input
+            placeholder="Folder Name"
+            type="text"
+            className="px-2 py-2"
+            {...register("name")}
+          />
+
+          {formState.errors.name && (
+            <p className="text-red-500 text-xs">
+              {formState.errors.name.message}
+            </p>
+          )}
+
+          {isError && <p className="text-red-500 text-xs">{errorMessage}</p>}
+          <div className="flex-row gap-2">
+            <Button
+              type="submit"
+              className="cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
+              disabled={isPending}
+            >
+              Create
+            </Button>
+            <DialogClose asChild>
+              <Button
+                variant="outline"
+                className="cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
+                disabled={isPending}
+              >
+                Cancel
+              </Button>
+            </DialogClose>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default NewFolderModal;
