0b902af243c5778483bf8f3bbe6f9181c5645b61
ZenithSUS <jeranmerino147@gmail.com>
2026-02-25 20:28:34 +0800

    add: new auth keys.

--- client/src/features/auth/auth.keys.ts
+++ client/src/features/auth/auth.keys.ts
@@ -1,6 +1,9 @@
 export const authKeys = {
   all: ["auth"] as const,
   user: () => [...authKeys.all, "user"] as const,
+  login: () => [...authKeys.all, "login"] as const,
+  register: () => [...authKeys.all, "register"] as const,
+  logout: () => [...authKeys.all, "logout"] as const,
 };
 
 export type AuthKeys = typeof authKeys;
