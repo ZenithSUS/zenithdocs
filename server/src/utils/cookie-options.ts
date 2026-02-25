27642498305bbb9f6bd855f568da11a69b917d85
ZenithSUS <jeranmerino147@gmail.com>
2026-02-26 01:16:23 +0800

    modify: cookie options.

--- server/src/utils/cookie-options.ts
+++ server/src/utils/cookie-options.ts
@@ -1,19 +1,17 @@
 import { CookieOptions } from "express";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export const getRefreshTokenCookieOptions = (): CookieOptions => ({
   httpOnly: true,
-  secure: isProd,
-  sameSite: isProd ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   path: "/",
 });
 
 export const clearRefreshTokenCookieOptions = (): CookieOptions => ({
   httpOnly: true,
-  secure: isProd,
-  sameSite: isProd ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 0,
   path: "/",
 });
