1125ee6a1459f09b2ab16a06a00ca48610db8a0f
ZenithSUS <jeranmerino147@gmail.com>
2026-02-26 11:40:55 +0800

    add: refresh protect middleware on getting refresh token route.

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -7,6 +7,7 @@ import {
   refreshAccessTokenController,
   logoutController,
 } from "../controllers/auth.controller.js";
+import refreshProctect from "../middlewares/refresh-protect.js";
 
 const router = Router();
 
@@ -14,7 +15,7 @@ const router = Router();
 router.post("/login", loginController);
 router.post("/register", registerUserController);
 router.post("/logout", logoutController);
-router.post("/refresh", refreshAccessTokenController);
+router.post("/refresh", refreshProctect, refreshAccessTokenController);
 
 // Get current user
 router.get("/me", protect, getMeController);
