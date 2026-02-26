2c600c8dcde78f5a362d7d836b64b58f329af6ad
ZenithSUS <jeranmerino147@gmail.com>
2026-02-26 16:48:44 +0800

    modify: tanstack configuration.

--- client/src/lib/tanstack.ts
+++ client/src/lib/tanstack.ts
@@ -3,12 +3,12 @@ import { QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
-      gcTime: 24 * 60 * 60 * 1000, // 24 hours
+      staleTime: 5 * 60 * 1000, // data fresh for 5 mins, no refetch
+      gcTime: 24 * 60 * 60 * 1000, // keep cache for 24 hours
       retry: false,
-      refetchOnMount: false,
-      refetchOnWindowFocus: true,
-      refetchOnReconnect: true,
+      refetchOnMount: false, // don't refetch when component mounts
+      refetchOnWindowFocus: false, // don't refetch on tab focus
+      refetchOnReconnect: true, // refetch on reconnect
     },
     mutations: {
       retry: false,
