d30b9d15a25c4547eff3950d206d206fc6967da3
ZenithSUS <jeranmerino147@gmail.com>
2026-02-28 17:39:42 +0800

    chore: remove unused modal comments
    - Removed commented out modal section
    - Cleans up code for better readability

--- client/src/components/dashboard/tabs/Folders.tsx
+++ client/src/components/dashboard/tabs/Folders.tsx
@@ -129,8 +129,6 @@ function FolderDashBoard({
 
   return (
     <div className="space-y-5">
-      {/* Modals */}
-
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {allFolders.map((folder) => {
           const docs = getDocsForFolder(folder._id);
