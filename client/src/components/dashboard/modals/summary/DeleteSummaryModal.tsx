1b07993fd74031f65e3154418072125d3cb45156
ZenithSUS <jeranmerino147@gmail.com>
2026-03-01 20:48:07 +0800

    feat(dashboard): add delete summary modal
    - Implemented modal for summary deletion
    - Added confirmation dialog and delete action
    - Integrated with summary deletion API

---
+++ client/src/components/dashboard/modals/summary/DeleteSummaryModal.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { dashboardKeys } from "@/features/dashboard/dashboard.keys";
+import useSummary from "@/features/summary/useSummary";
+import { AxiosError } from "@/types/api";
+import { useQueryClient } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
+import { useCallback } from "react";
+import { toast } from "sonner";
+
+interface DeleteSummaryModalProps {
+  userId: string;
+  summaryId: string;
+  documentId: string;
+}
+
+function DeleteSummaryModal({
+  userId,
+  summaryId,
+  documentId,
+}: DeleteSummaryModalProps) {
+  const queryClient = useQueryClient();
+  const { deleteSummaryMutation } = useSummary(userId, documentId, summaryId);
+  const { mutateAsync: deleteSummary } = deleteSummaryMutation;
+
+  const handleDelete = useCallback(async () => {
+    try {
+      await deleteSummary(summaryId);
+      await queryClient.refetchQueries({
+        queryKey: dashboardKeys.overview(),
+      });
+      toast.success("Summary deleted successfully!");
+    } catch (error) {
+      const err = error as AxiosError;
+      toast.error(err.response?.data?.message || "Error deleting summary");
+    }
+  }, [deleteSummary, summaryId]);
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Trash2 size={20} className="text-red-600 cursor-pointer" />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            summary.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteSummaryModal;
