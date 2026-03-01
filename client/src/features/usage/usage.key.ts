7d023c5cbfc88681f86810f67060f3c15722c3c9
ZenithSUS <jeranmerino147@gmail.com>
2026-03-01 18:06:00 +0800

    feat(usage): add key for user usage over six months

--- client/src/features/usage/usage.key.ts
+++ client/src/features/usage/usage.key.ts
@@ -3,6 +3,8 @@ const usageKeys = {
   byUser: (userId: string) => [...usageKeys.all, userId] as const,
   byUserAndMonth: (userId: string, month: string) =>
     [...usageKeys.byUser(userId), month] as const,
+  byUserSixMonths: (userId: string) =>
+    [...usageKeys.byUser(userId), "six"] as const,
 };
 
 export type UsageKeys = typeof usageKeys;
