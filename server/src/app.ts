822103beba3a33c8c11462cb0406ea314469dbe0
ZenithSUS <jeranmerino147@gmail.com>
2026-03-01 18:07:06 +0800

    feat(server): increase JSON body limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -56,7 +56,7 @@ app.use(
     optionsSuccessStatus: 204,
   }),
 );
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(hpp());
