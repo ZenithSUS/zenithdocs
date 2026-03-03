b981f6a869a7213af715919664fa8e9dc4ef26bc
ZenithSUS <jeranmerino147@gmail.com>
2026-03-03 15:32:09 +0800

    refactor(user): remove rate limiting from routes
    - Removed rate limit middleware
    - Simplified route definitions
    - Reduced code complexity

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -8,41 +8,14 @@ import {
 import protect from "../middlewares/protect.middleware.js";
 import authorizeSelfOrAdmin from "../middlewares/authorize-self-or-admin.middleware.js";
 import requireAdmin from "../middlewares/require-admin.middleware.js";
-import rateLimit from "../middlewares/ratelimit.middleware.js";
 
 const router = Router();
 
 // User routes
-router.get(
-  "/",
-  protect,
-  requireAdmin,
-  rateLimit("fetchUsersAdmin"),
-  getAllUsersController,
-);
+router.get("/", protect, requireAdmin, getAllUsersController);
+router.get("/:id", protect, authorizeSelfOrAdmin, getUserByIdController);
 
-router.get(
-  "/:id",
-  protect,
-  authorizeSelfOrAdmin,
-  rateLimit("fetchUser"),
-  getUserByIdController,
-);
-
-router.put(
-  "/:id",
-  protect,
-  authorizeSelfOrAdmin,
-  rateLimit("updateUser"),
-  updateUserController,
-);
-
-router.delete(
-  "/:id",
-  protect,
-  authorizeSelfOrAdmin,
-  rateLimit("deleteUser"),
-  deleteUserController,
-);
+router.put("/:id", protect, authorizeSelfOrAdmin, updateUserController);
+router.delete("/:id", protect, authorizeSelfOrAdmin, deleteUserController);
 
 export default router;
