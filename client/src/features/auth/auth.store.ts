e5d2a6915c96435a171ffd40c372d9482dbb623a
ZenithSUS <jeranmerino147@gmail.com>
2026-03-04 22:13:51 +0800

    feat(auth): add user ID to auth store

--- client/src/features/auth/auth.store.ts
+++ client/src/features/auth/auth.store.ts
@@ -1,11 +1,15 @@
 import { create } from "zustand";
 
 interface AuthStore {
+  userId: string | null;
+  setUserId: (id: string | null) => void;
   accessToken: string | null;
   setAccessToken: (token: string | null) => void;
 }
 
 const useAuthStore = create<AuthStore>((set) => ({
+  userId: null,
+  setUserId: (id) => set({ userId: id }),
   accessToken: null,
   setAccessToken: (token) => set({ accessToken: token }),
 }));
