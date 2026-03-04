e2934686eb8a1ad458028a181897809da06df2c3
ZenithSUS <jeranmerino147@gmail.com>
2026-03-04 20:24:10 +0800

    feat(socket): initialize and start Socket.IO server
    - Integrate HTTP server for Socket.IO
    - Initialize Socket.IO with allowed origins
    - Start server using HTTP server instance

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,24 @@ import config from "./config/env.js";
 import colors from "./utils/log-colors.js";
 import connectDB from "./config/db.js";
 
+// Socket IO Server
+import { createServer } from "http";
+import { initSocket } from "./config/socket.js";
+
+const httpServer = createServer(app);
+initSocket(httpServer, config.server.allowedOrigins.split(","));
+
 // Connect to MongoDB
 connectDB();
+
+// Start the HTTP server
+httpServer.listen(config.server.port, () => {
+  console.log("=".repeat(50));
+  console.log(
+    `${colors.blue}ZenithDocs Socket server listening on port ${config.server.port} ${colors.reset}`,
+  );
+  console.log("=".repeat(50));
+});
 
 // Start the server
 app.listen(config.server.port, () => {
