339031e27dfd266c9d0b118c1785513d8e4bc373
ZenithSUS <jeranmerino147@gmail.com>
2026-03-15 19:19:19 +0800

    build(server): containerize app with Dockerfile
    - Add multi-stage Dockerfile
    - Create .dockerignore for server
    - Consolidate server startup via httpServer

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,30 +3,20 @@ import config from "./config/env.js";
 import colors from "./utils/log-colors.js";
 import connectDB from "./config/db.js";
 
-// Socket IO Server
 import { createServer } from "http";
 import { initSocket } from "./config/socket.js";
 
 const httpServer = createServer(app);
 initSocket(httpServer, config.server.allowedOrigins.split(","));
 
-// Connect to MongoDB
 connectDB();
 
-// Start the HTTP server
-httpServer.listen(config.server.port, () => {
+// Start Server
+httpServer.listen(Number(config.server.port), "0.0.0.0", () => {
   console.log("=".repeat(50));
   console.log(
-    `${colors.blue}ZenithDocs Socket server listening on port ${config.server.port} ${colors.reset}`,
+    `${colors.green}ZenithDocs server listening on port ${config.server.port}${colors.reset}`,
   );
-  console.log("=".repeat(50) + "\n");
-});
-
-// Start the server
-app.listen(config.server.port, () => {
-  console.log("=".repeat(50));
-  console.log(
-    `${colors.green}ZenithDocs server listening on port ${config.server.port} ${colors.reset}`,
-  );
+  console.log(`${colors.blue}Socket.IO ready${colors.reset}`);
   console.log("=".repeat(50) + "\n");
 });
