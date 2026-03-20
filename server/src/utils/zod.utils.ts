6993099ca5970d5cea8f336ed41b975e755beb74
ZenithSUS <jeranmerino147@gmail.com>
2026-03-20 14:55:36 +0800

    feat(zod): add email and user token schemas
    - Add email validation schema
    - Introduce user token schema
    - Enhance object ID validation

--- server/src/utils/zod.utils.ts
+++ server/src/utils/zod.utils.ts
@@ -3,6 +3,16 @@ import z from "zod";
 
 export const objectId = z
   .string()
+  .min(1, "Object ID is required.")
   .refine((id) => mongoose.Types.ObjectId.isValid(id), {
     message: "Invalid object ID",
   });
+
+export const email = z
+  .string()
+  .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format.");
+
+export const userTokenSchema = z.object({
+  userId: objectId,
+  role: z.enum(["user", "admin"]),
+});
